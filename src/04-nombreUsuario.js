87d1490efa2f899fa8dd116f69afbd17d967a59b
Nair Paez <nairpaez@outlook.com>
2026-03-06 11:43:04 -0300

    ejercicio-04- mostrar nombre de usuario

---
+++ src/04-nombreUsuario.js
@@ -0,0 +1,5 @@
+// * 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+
+const nameUser= prompt('Ingrese su nombre')
+
+document.writeln( "<span class='ms-5 text-secundary fs-3'> ¡Hola " + nameUser + "!</span>" )
