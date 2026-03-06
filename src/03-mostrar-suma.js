89e8acbab394d87b16f452b52e490b72299fea43
Nair Paez <nairpaez@outlook.com>
2026-03-06 11:34:53 -0300

    ejercicio-03- mostrar suma de 2 numeros

---
+++ src/03-mostrar-suma.js
@@ -0,0 +1,3 @@
+// * 3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
+document.writeln( "<span class='ms-5 text-danger fs-1'>" + (3 + 5) + "</span>" )
